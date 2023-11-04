101c90b5245e2bc76a7f50615abcbb1c5ddc0f6e
paulomon <paulomontanger@gmail.com>
2023-11-04 14:14:21 -0300

    Implementado tratamento de erro quando cliente não encontrado (#13)

--- src/main/kotlin/tech/challenge/fiap/authfunction/domain/AuthFunctionService.kt
+++ src/main/kotlin/tech/challenge/fiap/authfunction/domain/AuthFunctionService.kt
@@ -2,9 +2,11 @@ package tech.challenge.fiap.authfunction.domain
 
 import com.google.firebase.auth.FirebaseAuth
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.function.adapter.gcp.FunctionInvoker
 import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
-import java.lang.RuntimeException
 import java.util.function.Function
 
 @Component
@@ -17,23 +19,35 @@ class AuthFunctionService {
     lateinit var firebaseAuth: FirebaseAuth
 
     @Bean
-    fun auth(): Function<AuthRequestDto, TokenDto> {
+    fun auth(): Function<AuthRequestDto, Message<*>> {
         return Function {
-            val cliente = userRepository.findClienteByCpf(it.cpf) ?: throw RuntimeException("Usuário não encontrado")
+            val cliente = userRepository.findClienteByCpf(it.cpf) ?: kotlin.run {
+                return@Function buildClienteNotFoundResponse()
+            }
 
             val customClaims = mapOf(
-                Pair("id", cliente.id),
                 Pair("nome", cliente.nome),
-                Pair("email", cliente.email),
                 Pair("cpf", cliente.cpf)
             )
 
-            val cpf = it.cpf.filter { c -> c.isDigit() }
-            val token = firebaseAuth.createCustomToken(cpf, customClaims)
+            return@Function firebaseAuth.createCustomToken(cliente.cpf, customClaims).let {
+                buildTokenResponse(it)
+            }
+        }
+    }
 
-            return@Function TokenDto(
+    private fun buildTokenResponse(token: String): Message<TokenDto> {
+        return MessageBuilder.withPayload(
+            TokenDto(
                 jwt = token
             )
-        }
+        ).build()
+    }
+
+    private fun buildClienteNotFoundResponse(): Message<String> {
+        return MessageBuilder
+            .withPayload("Cliente não encontrado")
+            .setHeader(FunctionInvoker.HTTP_STATUS_CODE, 404)
+            .build()
     }
 }
